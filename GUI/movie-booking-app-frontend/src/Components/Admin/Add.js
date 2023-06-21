2d8da28bf7035882ac66c48d1493af8152811480
reputationabhi <abhi007.kv@gmail.com>
2023-06-22 02:05:19 +0530

    Added Frontend Styling to Admin element (Add.js) and fixed indentations on Authentication elements

--- GUI/movie-booking-app-frontend/src/Components/Admin/Add.js
+++ GUI/movie-booking-app-frontend/src/Components/Admin/Add.js
@@ -1,91 +1,172 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import useStore from '../../StateStorage';
+import React, { useState } from "react";
+import axios from "axios";
+import useStore from "../../StateStorage";
 
 const Add = () => {
+  const [message, setMessage] = useState("");
+  const [showResult, setShowResult] = useState(false);
 
-    const [message, setMessage] = useState("");
-    const [showResult, setShowResult] = useState(false);
+  const { storedJwtToken } = useStore((state) => ({
+    storedJwtToken: state.jwtToken,
+  }));
 
-    const { storedJwtToken } = useStore(
-        (state) => ({
-            storedJwtToken: state.jwtToken
-        })
-    )
-
-    const handleSubmit = async (event) => {
-        setShowResult(false);
-        setMessage("");
-        event.preventDefault();
-        const data = {
-            "name": event.target.name.value,
-            "theatreName": event.target.theatreName.value,
-            "ticketsAlloted": event.target.ticketsAlloted.value,
-            "ticketsBooked": event.target.ticketsBooked.value,
-        }
-        setShowResult(true);
-        // event.target.reset();
-        await axios.post('https://localhost:7222/api/MovieBooking/movie/add', data
-            ,
-            {
-                headers: {
-                    'Authorization': 'Bearer ' + storedJwtToken,
-                }
-            })
-            .then((response) => {
-                setMessage("Movie Added Sucessfully");
-            })
-            .catch(error => {
-                setMessage("Some Error Occured");
-            });
+  const handleSubmit = async (event) => {
+    setShowResult(false);
+    setMessage("");
+    event.preventDefault();
+    const data = {
+      name: event.target.name.value,
+      theatreName: event.target.theatreName.value,
+      ticketsAlloted: event.target.ticketsAlloted.value,
+      ticketsBooked: event.target.ticketsBooked.value,
     };
+    setShowResult(true);
+    // event.target.reset();
+    await axios
+      .post("https://localhost:7222/api/MovieBooking/movie/add", data, {
+        headers: {
+          Authorization: "Bearer " + storedJwtToken,
+        },
+      })
+      .then((response) => {
+        setMessage("Movie Added Sucessfully");
+      })
+      .catch((error) => {
+        setMessage("Some Error Occured");
+      });
+  };
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <h1>Add Movies</h1>
-                <br />
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label>Name</label>
-                            </td>
-                            <td>
-                                <input type='text' name='name' placeholder='name'></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Theatre Name</label>
-                            </td>
-                            <td>
-                                <input type='text' name='theatreName' placeholder='theatreName'></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Tickets Alloted</label>
-                            </td>
-                            <td>
-                                <input type='number' name='ticketsAlloted' placeholder='ticketsAlloted'></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Tickets Booked</label>
-                            </td>
-                            <td>
-                                <input type='number' name='ticketsBooked' placeholder='ticketsBooked'></input>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <input type='submit' value='Add Movie'></input>
-            </form>
-            {showResult && message}
-        </>
-    )
-}
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundImage: "url('background-image.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    padding: "20px",
+    fontFamily: "Arial, sans-serif",
+  };
+
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    padding: "40px",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+  };
+
+  const titleStyle = {
+    marginBottom: "20px",
+    fontSize: "24px",
+    fontWeight: "bold",
+    color: "#333",
+  };
+
+  const inputStyle = {
+    padding: "10px",
+    borderRadius: "3px",
+    border: "1px solid #ccc",
+    width: "300px",
+    marginBottom: "20px",
+    fontSize: "14px",
+  };
+
+  const buttonStyle = {
+    padding: "10px 20px",
+    backgroundColor: "#4caf50",
+    color: "white",
+    border: "none",
+    borderRadius: "3px",
+    cursor: "pointer",
+    fontSize: "14px",
+    fontWeight: "bold",
+    transition: "background-color 0.3s",
+  };
+
+  const buttonHoverStyle = {
+    backgroundColor: "#45a049",
+  };
+
+  const messageStyle = {
+    marginTop: "20px",
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "#4caf50",
+  };
+
+  return (
+    <>
+      <div style={containerStyle}>
+        <form style={formStyle} onSubmit={handleSubmit}>
+          <h1 style={titleStyle}>Add Movies</h1>
+          <br />
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label>Name</label>
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="name"
+                    style={inputStyle}
+                    placeholder="Name"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Theatre Name</label>
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="theatreName"
+                    style={inputStyle}
+                    placeholder="Theatre Name"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Tickets Alloted</label>
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    name="ticketsAlloted"
+                    style={inputStyle}
+                    placeholder="Tickets Alloted"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Tickets Booked</label>
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    name="ticketsBooked"
+                    style={inputStyle}
+                    placeholder="Tickets Booked"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <input type="submit" value="Add Movie" style={buttonStyle} />
+        </form>
+        {showResult && <p style={messageStyle}>{message}</p>}
+      </div>
+    </>
+  );
+};
 
 export default Add;
--- GUI/movie-booking-app-frontend/src/Components/Authentication/Register.js
+++ GUI/movie-booking-app-frontend/src/Components/Authentication/Register.js
@@ -95,121 +95,149 @@
 
 // export default Register;
 
-
-
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [message, setMessage] = useState("");
+  const [showResult, setShowResult] = useState(false);
 
-    const [message, setMessage] = useState("");
-    const [showResult, setShowResult] = useState(false);
+  const handleSubmit = async (event) => {
+    setShowResult(false);
+    setMessage("");
+    event.preventDefault();
+    const data = {
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
+      email: event.target.email.value,
+      loginId: event.target.loginId.value,
+      password: event.target.password.value,
+      contact: event.target.contactNo.value,
+    };
+    setShowResult(true);
+    // event.target.reset();
+    await axios
+      .post("https://localhost:7222/api/MovieBooking/Register", data)
+      .then((response) => {
+        setMessage("User Created Successfully");
+      })
+      .catch((error) => {
+        setMessage("Some Error Occured");
+      });
+  };
 
-    const handleSubmit = async (event) => {
-        setShowResult(false);
-        setMessage("");
-        event.preventDefault();
-        const data = {
-            "firstName": event.target.firstName.value,
-            "lastName": event.target.lastName.value,
-            "email": event.target.email.value,
-            "loginId": event.target.loginId.value,
-            "password": event.target.password.value,
-            "contact": event.target.contactNo.value
-        }
-        setShowResult(true);
-        // event.target.reset();
-        await axios.post('https://localhost:7222/api/MovieBooking/Register', data)
-        .then((response) => {
-            setMessage("User Created Successfully");
-        })
-        .catch(error => {
-            setMessage("Some Error Occured");
-        });
-    };
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundImage: "url('background-image.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    padding: "20px",
+    fontFamily: "Arial, sans-serif",
+  };
 
-    const containerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        backgroundImage: "url('background-image.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        padding: '20px',
-        fontFamily: 'Arial, sans-serif',
-    };
-    
-    const formStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-        padding: '40px',
-        borderRadius: '5px',
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-    };
-    
-    const titleStyle = {
-        marginBottom: '20px',
-        fontSize: '24px',
-        fontWeight: 'bold',
-        color: '#333',
-    };
-    
-    const inputStyle = {
-        padding: '10px',
-        borderRadius: '3px',
-        border: '1px solid #ccc',
-        width: '300px',
-        marginBottom: '20px',
-        fontSize: '14px',
-    };
-    
-    const buttonStyle = {
-        padding: '10px 20px',
-        backgroundColor: '#4caf50',
-        color: 'white',
-        border: 'none',
-        borderRadius: '3px',
-        cursor: 'pointer',
-        fontSize: '14px',
-        fontWeight: 'bold',
-        transition: 'background-color 0.3s',
-    };
-    
-    const buttonHoverStyle = {
-        backgroundColor: '#45a049',
-    };
-    
-    const messageStyle = {
-        marginTop: '20px',
-        fontSize: '18px',
-        fontWeight: 'bold',
-        color: '#4caf50',
-    };
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    padding: "40px",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+  };
 
-    return (
-        <>
-        <div style={containerStyle}>
+  const titleStyle = {
+    marginBottom: "20px",
+    fontSize: "24px",
+    fontWeight: "bold",
+    color: "#333",
+  };
+
+  const inputStyle = {
+    padding: "10px",
+    borderRadius: "3px",
+    border: "1px solid #ccc",
+    width: "300px",
+    marginBottom: "20px",
+    fontSize: "14px",
+  };
+
+  const buttonStyle = {
+    padding: "10px 20px",
+    backgroundColor: "#4caf50",
+    color: "white",
+    border: "none",
+    borderRadius: "3px",
+    cursor: "pointer",
+    fontSize: "14px",
+    fontWeight: "bold",
+    transition: "background-color 0.3s",
+  };
+
+  const buttonHoverStyle = {
+    backgroundColor: "#45a049",
+  };
+
+  const messageStyle = {
+    marginTop: "20px",
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "#4caf50",
+  };
+
+  return (
+    <>
+      <div style={containerStyle}>
         <form style={formStyle} onSubmit={handleSubmit}>
-        <h1 style={titleStyle}>Register Page</h1>
-        <input style={inputStyle} type="text" name="firstName" placeholder="First Name" />
-        <input style={inputStyle} type="text" name="lastName" placeholder="Last Name" />
-        <input style={inputStyle} type="email" name="email" placeholder="Email" />
-        <input style={inputStyle} type="text" name="loginId" placeholder="Username" />
-        <input style={inputStyle} type="password" name="password" placeholder="Password" />
-        <input style={inputStyle} type="text" name="contactNo" placeholder="Contact No." />
-        <button style={buttonStyle} type="submit">
-        Register
-        </button>
-    </form>
-    {showResult && <p style={messageStyle}>{message}</p>}
-    </div>
-        </>
-    );
+          <h1 style={titleStyle}>Register Page</h1>
+          <input
+            style={inputStyle}
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+          />
+          <input
+            style={inputStyle}
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+          />
+          <input
+            style={inputStyle}
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            style={inputStyle}
+            type="text"
+            name="loginId"
+            placeholder="Username"
+          />
+          <input
+            style={inputStyle}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            style={inputStyle}
+            type="text"
+            name="contactNo"
+            placeholder="Contact No."
+          />
+          <button style={buttonStyle} type="submit">
+            Register
+          </button>
+        </form>
+        {showResult && <p style={messageStyle}>{message}</p>}
+      </div>
+    </>
+  );
 };
 
 export default Register;
